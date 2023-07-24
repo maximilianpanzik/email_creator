465a8a70492d920d769bf1f90de47fdb3e507229
mp <maximilianpanzik@gmail.com>
2023-07-24 07:49:10 +0200

    erster erfolgreicher test

--- test.py
+++ test.py
@@ -1,4 +1,33 @@
 import pandas as pd
+import os
 excel_file = "data_list.xlsx"
+csv_file = "data_list.csv"
 #df = pd.read_excel(excel_file)
-pd.read_excel(excel_file, index_col=None, header=1)
+df = pd.read_csv(csv_file, delimiter=";")
+print(df)
+column_names = list(df.columns)
+#print(list(df.columns))
+with open('mail_template.txt') as f:
+    template = f.read()
+
+for ind in df.index:
+    lines = template
+    for col in column_names:
+        value = df[col][ind]
+        if pd.isna(value):
+            value = 'error'
+        else:
+            value = str(value)
+
+        print(value)
+
+        lines = lines.replace(col, value)
+        print("test")
+
+    print(lines)
+    mail_address = (df["mail-address"][ind]).replace(".","_")
+    filename = f'mail_output/{mail_address}_Bestellnummer_.txt'
+
+    with open(filename, 'w') as f:
+        f.write(lines)
+
